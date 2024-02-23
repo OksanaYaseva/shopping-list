82d5c1913e4da5b87c6756301dde144cd37309bc
OksanaYaseva <o-reyn@mail.ru>
2024-02-23 22:38:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const sendInput = document.querySelector('#input');
+const items = document.querySelector('.items');
+
+
+sendInput.addEventListener('keydown', function (event) {
+    if (event.key == 'Enter') {
+        const itemsText = sendInput.value;
+
+        const newMessage = document.createElement('div');
+        newMessage.classList.add('element')
+        newMessage.textContent = itemsText;
+        newMessage.addEventListener('click', function () {
+            newMessage.classList.toggle('done');
+        })
+
+        if (itemsText != '') {
+            items.append(newMessage)
+        }
+
+        sendInput.value = '';
+
+    }
+});
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
